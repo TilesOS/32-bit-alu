bab939f36642799804660763a3496dd61ec6243b
Tyler McClure <tylermcclure11@gmail.com>
2026-06-09 12:22:18 -0400

    packcage for alu and testbench

---
+++ alu_pkg.sv
@@ -0,0 +1,18 @@
+package alu_pkg;
+
+    parameter int DATA_WIDTH = 32;
+
+    typedef enum logic [3:0] {
+        ALU_ADD,
+        ALU_SUB,
+        ALU_AND,
+        ALU_OR,
+        ALU_XOR,
+        ALU_SLL,
+        ALU_SRL,
+        ALU_SRA,
+        ALU_SLT,
+        ALU_SLTU
+    } alu_op_t;
+
+endpackage
